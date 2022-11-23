c8cb47f6dcc65ddbfaa5a027a73eb044a468c7c3
zxcvbzzy1 <98303335+zxcvbzzy1@users.noreply.github.com>
2022-11-23 13:58:47 +0800

    Add files via upload

---
+++ js/changeSrc.js
@@ -0,0 +1,32 @@
+export let srcContent=["images/svg/search-line1.svg",
+        "images/svg/user1.svg",
+        "images/svg/home 11-fill.svg",
+        "images/svg/menu1.svg",]
+export let srcContent1=["images/svg/search-line.svg",'images/svg/user.svg',
+        'images/svg/home%201-fill.svg','images/svg/menu.svg',]
+export let srcContact=[
+    'images/svg/wechat.svg','images/svg/QQ.svg',
+    'images/svg/QQ空间black.svg','images/svg/bilibili.svg',
+    'images/svg/朋友圈.svg',
+]
+export let srcContact1=[
+    'images/svg/wechat1.svg','images/svg/QQ1.svg',
+    'images/svg/QQ空间.svg','images/svg/bilibili1.svg',
+    'images/svg/朋友圈1.svg'
+]
+export function change_Src(arr,eve,index){
+        eve.src=arr[index];
+}
+export function svgSrc (event,menuHover){
+    if(event.target.closest('td'))
+    {
+        change_Src(srcContent,event.target,+event.target.dataset.name)
+        event.target.onclick=function (){
+            if(event.target.dataset.name==='3')
+            {
+                menuHover.classList.toggle('open');
+            }
+        }
+        event.target.onmouseleave=function (){
+            change_Src(srcContent1,event.target,+event.target.dataset.name)
+        }}}
